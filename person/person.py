c3fb43ca07bb5e2f915848a1c856c42c3c34be01
João Paulo <jbarretodasilva7@gmail.com>
2025-08-25 23:21:28 -0400

    apaguei o antigo nome da classe person

--- person/person.py
+++
@@ -1,9 +0,0 @@
-#classe principal do jogador
-
-class Jogador:
-    
-    def __init__(self, nome, saldo): # construção da classe recebendo o nome e o saldo inicial do jogador
-        self.nome = nome
-        self.saldo = saldo
-
-    
