10db3d4bcfc23efb4099baf434ff46a86aa820f2
João Paulo <jbarretodasilva7@gmail.com>
2025-08-25 22:17:35 -0400

    criação da classe do Jogador

---
+++ person/person.py
@@ -0,0 +1,9 @@
+#classe principal do jogador
+
+class Jogador:
+    
+    def __init__(self, nome, saldo): # construção da classe recebendo o nome e o saldo inicial do jogador
+        self.nome = nome
+        self.saldo = saldo
+
+    
