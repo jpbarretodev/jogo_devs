5a8580870c10e1ee3d7ea417fa5e582239428d56
João Paulo <jbarretodasilva7@gmail.com>
2025-08-28 18:16:00 -0400

    atualização dos codigos

--- person/maquina.py
+++ person/maquina.py
@@ -1,9 +1,34 @@
 # Classe exclusiva para a Máquina
+import random                       # importação da biblioteca para realizar a randomização da escolha dos elementos
+
 
 class Maquina:
 
     def __init__(self):
         pass
 
+    @staticmethod
+    def contador_de_jogadas():
+        combinacao = [[1, 1, 1], [2, 2, 2], [3, 3, 3]]
+        contador = 0
+        while True:
+            jogada = Maquina.gerar_combinacao()  # <-- chama a função a cada loop
+            contador += 1
+            if jogada in combinacao:
+                break
+
+        return print(contador)
+
+
     def realizar_aposta(saldo_de_aposta):
-        
+        pass
+
+    @staticmethod
+    def gerar_combinacao():
+        lista = []
+        for i in range(3):
+            lista.append(random.randint(1, 3))
+            #print(lista)
+        return lista
+
+Maquina.contador_de_jogadas()
