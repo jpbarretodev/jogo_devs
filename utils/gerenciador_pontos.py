4a448c74c3fc3e9b8ce4e0b764b74f8a7e2b1ca1
João Paulo <jbarretodasilva7@gmail.com>
2025-08-31 16:58:30 -0400

    criação do código de manipulação de pontos

---
+++ utils/gerenciador_pontos.py
@@ -0,0 +1,38 @@
+# código feito exclusivamente para gerenciar os pontos
+import json
+
+caminho_combinacao = "utils/combinacoes_vencedoras.json"
+caminho_pontos = "utils/multiplicador_pontos.json"
+
+combinacoes_possiveis = []
+pontuacoes = []
+
+# abrir arquivos - combinacoes possíveis
+with open(caminho_combinacao, "r") as file:
+    combinacoes = json.load(file)
+
+     # gerar lista de combinacoes
+    for i in range(len(combinacoes)):
+        combinacoes_possiveis.append(combinacoes["combinacao{}".format(i+1)])
+
+with open(caminho_pontos, "r") as file:
+    pontos = json.load(file)
+
+    #gera lista dos pontos
+    for i in range(len(pontos)):
+        pontuacoes.append(pontos["combinacao{}".format(i+1)])
+
+#################################################################################
+
+def gerar_pontuacao(jogada_escolhida):
+    if jogada_escolhida in combinacoes_possiveis:
+        for indice, elemento in enumerate(combinacoes_possiveis):
+            if jogada_escolhida == elemento:
+                #return indice
+                multiplicador_ponto = pontuacoes[indice]
+                return print(3 * multiplicador_ponto) # retorna o saldo final do jogador na aposta
+
+
+print(combinacoes_possiveis)
+print(pontuacoes)
+gerar_pontuacao([3, 3, 3])
