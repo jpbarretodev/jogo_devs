bb2f13588eeeac9efa93bc0bbeaef9fec0668359
João Paulo <jbarretodasilva7@gmail.com>
2025-09-06 11:48:09 -0400

    Fiz o primeiro teste do código usando as classes fazendo apostar únicas

--- tests/teste_funcionalidade_classe.py
+++ tests/teste_funcionalidade_classe.py
@@ -1,7 +1,25 @@
 # Codigo exclusivo para testar a classe
 
 from person.jogador import Jogador
+from person.maquina import Maquina
+from utils.gerenciador_pontos import gerar_pontuacao
 
-objeto = Jogador("joao", 200)
+# instanciando os objetos
+m = Maquina()
+user = Jogador("Tester", 100)
 
-objeto.apostar()
+# primeiro passo do jogo: usuário insere o valor da aposta
+user.apostar()
+
+# segundo passo do jogo: maquina gera a jogada e retorna a lista para o front
+lista = m.aposta(user.apostar()) # recebe TODA a jogada
+
+# terceiro passo do jogo: precisa passar para o GERADOR DE PONTUAÇÃO a jogada e o saldo inserido
+result = gerar_pontuacao(lista[len(lista) - 1], user.saldo_inserido)
+
+# Quarto passo: alterar o saldo do usuário após aposta
+user.saldo += result
+
+
+# Verificar estado atual do jogador
+print("Saldo atual: {}\nSaldo inserido: {}\nNovo saldo (valor aposta + multiplicador): {}".format(user.saldo, user.saldo_inserido, result))
