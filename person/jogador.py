149e07e28499baa9824117700493d1bbdf22be9a
João Paulo <jbarretodasilva7@gmail.com>
2025-09-06 11:47:22 -0400

    Alterei o método de Apostar do jogador e inseri o atributo de saldo inserido

--- person/jogador.py
+++ person/jogador.py
@@ -5,6 +5,7 @@ class Jogador:
     def __init__(self, nome: str, saldo: int): # construção da classe recebendo o nome e o saldo inicial do jogador
         self.nome = nome
         self.saldo = saldo # por enquanto, a varíavel receberá int
+        self.saldo_inserido = 0
 
     def verificar_saldo(self):
         if self.saldo <= 0:
@@ -12,14 +13,23 @@ class Jogador:
         else:
             return True
 
-    def apostar(self):
+    def apostar(self): # < -- FUNÇÃO EM TESTE -- >
         if self.verificar_saldo() == True:
-            print("Ok")
+            self.saldo_inserido = int(input("Insira o valor da aposta: ")) # SOMENTE PARA TESTE, DESENROLAR DEPOIS COM A GALERA DO FRONT
+
+            while self.saldo_inserido > self.saldo or self.saldo_inserido <= 0 :
+                self.saldo_inserido = int(input("Insira o valor da aposta novamente: "))
+            self.saldo -= self.saldo_inserido # diminui o saldo do usuário mediante inserção do saldo de aposta            
+
         else:
             print("Você perdeu por estar sem saldo!")
 
     def alterar_saldo(self):
-        
         pass
 
-user = Jogador("Tester", 100)
+'''
+user = Jogador("Tester", 2)
+print(f"Saldo atual: {user.saldo}")
+user.apostar()
+print(f"Saldo após a aposta: {user.saldo} e Saldo inserido: {user.saldo_inserido}")
+'''
